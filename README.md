14f5e881a1593d44e14b4df44b2cac6c61a3630c
Sebastien Binet <binet@cern.ch>
2015-09-09 21:16:54 +0200

    all: first import

---
+++ README.md
@@ -0,0 +1,14 @@
+asdf
+====
+
+`asdf` is a (WIP) pure-Go implementation of the Advanced Scientific Data Format
+(ASDF) as described by the [ASDF standard](https://github.com/spacetelescope/asdf-standard).
+
+## Installation
+
+Use `go get`:
+
+```sh
+sh> go get github.com/astrogo/asdf
+```
+
---
+++ asdf.go
@@ -0,0 +1,42 @@
+// Copyright 2015 The astrogo Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package asdf
+
+// File holds informations about an ASDF file
+type File struct {
+	Version  string // low-level file format version
+	Comments []string
+	Tree     *Tree
+	Blocks   []Block
+	Index    *Index
+}
+
+// Tree holds structured informations and meta-data
+type Tree struct {
+}
+
+// Block represents a contiguous chunk of binary data in an ASDF file
+type Block struct {
+	Header Header
+	Data   []byte
+}
+
+// Header holds informations a Block
+type Header struct {
+	Magic       [4]byte // block magic token ("\323BLK")
+	Size        uint16  // size of the remainder of the header (not counting Magic nor Size)
+	Flags       uint32
+	Compression [4]byte
+	AllocSize   uint64   // amount of space allocated for the block (not including the header), in bytes
+	UsedSize    uint64   // amount of used space for the block on disk (not including the header), in bytes
+	DataSize    uint64   // size of the block when decoded, in bytes
+	Checksum    [16]byte // MD5 checksum of the used data in the block
+}
+
+// Index contains pointers to blocks in the form of pairs of
+// byte-offsets (begin, end) inside an ASDF file
+type Index struct {
+	Data [][2]int
+}
