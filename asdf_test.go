d2d4463b460adbcead41f6c5f95d9677a4741a39
Sebastien Binet <binet@cern.ch>
2015-09-17 12:15:29 +0200

    test: run TestReader on all testdata files

--- asdf_test.go
+++ asdf_test.go
@@ -10,21 +10,39 @@ import (
 	"testing"
 )
 
+var files = []string{
+	"testdata/ascii.asdf",
+	"testdata/basic.asdf",
+	"testdata/complex.asdf",
+	"testdata/compressed.asdf",
+	"testdata/exploded0000.asdf",
+	"testdata/exploded.asdf",
+	"testdata/float.asdf",
+	"testdata/int.asdf",
+	"testdata/shared.asdf",
+	"testdata/stream.asdf",
+	"testdata/unicode_bmp.asdf",
+	"testdata/unicode_spp.asdf",
+}
+
 func TestReader(t *testing.T) {
-	r, err := os.Open("testdata/ascii.asdf")
-	if err != nil {
-		t.Fatalf("could not open file: %v\n", err)
-	}
-	defer r.Close()
+	for _, fname := range files {
+		r, err := os.Open(fname)
+		if err != nil {
+			t.Fatalf("could not open file [%s]: %v\n", fname, err)
+		}
+		defer r.Close()
 
-	f, err := Open(r)
-	if err != nil {
-		t.Fatalf("could not open asdf file: %v\n", err)
-	}
+		f, err := Open(r)
+		if err != nil {
+			t.Fatalf("could not open asdf file [%s]: %v\n", fname, err)
+		}
 
-	fmt.Printf("f.version=%q\n", f.Version)
-	fmt.Printf("f.comments=%v\n", f.Comments)
-	fmt.Printf("f.tree=%v\n", f.Tree)
-	fmt.Printf("f.blocks=%v\n", f.Blocks)
-	fmt.Printf("f.index=%v\n", f.Index)
+		fmt.Printf("file=%q\n", fname)
+		fmt.Printf("f.version=%q\n", f.Version)
+		fmt.Printf("f.comments=%v\n", f.Comments)
+		fmt.Printf("f.tree=%v\n", f.Tree)
+		fmt.Printf("f.blocks=%v\n", f.Blocks)
+		fmt.Printf("f.index=%v\n", f.Index)
+	}
 }
