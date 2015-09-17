1451067dd939dd57113bb5675f5afced48175da1
Sebastien Binet <binet@cern.ch>
2015-09-17 10:56:33 +0200

    gen-schemas: first stab at generating go types from schemas

---
+++ gen-schemas.go
@@ -0,0 +1,63 @@
+//+build ignore
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type Schema struct {
+	ID         string `yaml:"id"`
+	Title      string
+	Tag        string
+	Type       string
+	Properties Properties
+	Required   []string
+	ExtraProps bool `yaml:"additionalProperties"`
+}
+
+type Properties map[string]Property
+
+func (props *Properties) UnmashalYAML(unmarshal func(interface{}) error) error {
+	return unmarshal(props)
+}
+
+type Property struct {
+	Description string
+	Type        string
+	Format      string
+}
+
+func main() {
+	name := os.Args[1]
+	buf, err := ioutil.ReadFile(name)
+	if err != nil {
+		panic(err)
+	}
+	var schema Schema
+	err = yaml.Unmarshal(buf, &schema)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("properties=%v\n", schema.Properties)
+
+	fmt.Printf("\n\n/////\n")
+	tag := strings.Split(schema.Tag, "/")
+	tname := strings.Title(strings.Split(tag[len(tag)-1], "-")[0])
+	fmt.Printf("type %s struct {\n", tname)
+	for k, prop := range schema.Properties {
+		fmt.Printf("\t%s %v `yaml:\"%s\"` // %q\n",
+			strings.Title(k),
+			prop.Type, k,
+			strings.TrimSpace(prop.Description),
+		)
+		//fmt.Printf("// %#v\n", prop)
+	}
+	fmt.Printf("}\n")
+}
